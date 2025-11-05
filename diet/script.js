a7e40213a0bbcc501d6cc366f57d7c639438fffa
edsonvsantos <edsonvsantos.235@gmail.com>
2025-11-05 08:18:20 -0300

    Update script.js

--- diet/script.js
+++ diet/script.js
@@ -1,4 +1,4 @@
-const apiKey = "4ta7q3VUgJWJzNpU32dxDkVGhsasDbglvf6PRXBA";
+const apiKey = "";
 let refeicoes = [];
 let totalGeral = 0;
 let modal;
@@ -153,3 +153,4 @@ function removerRefeicao(index) {
     renderizarRefeicoes();
   }
 }
+
